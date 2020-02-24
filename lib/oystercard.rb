cd8365d5176a707fc98f78d95af89200c0f34e42
Ariane <xu.ariane@hotmail.fr>
2020-02-24 17:08:01 +0000

    oyster

---
+++ lib/oystercard.rb
@@ -0,0 +1,22 @@
+class Oystercard
+
+  MAXIMUM_BALANCE = 90
+
+  attr_reader :balance
+
+
+  def initialize
+    @balance = 0
+  end
+
+  def top_up(amount)
+    maximum_balance = Oystercard::MAXIMUM_BALANCE
+    fail "Maximum balance of #{maximum_balance} exceeded" if amount + balance > MAXIMUM_BALANCE
+    @balance += amount
+  end
+
+  def deduct(amount)
+    @balance -= amount
+  end
+
+end
