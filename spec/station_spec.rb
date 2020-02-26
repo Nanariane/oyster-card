46098763d68bd5285b52cf2c7493890bf52a5b2a
Ariane <xu.ariane@hotmail.fr>
2020-02-26 16:03:56 +0000

    create station class pass test

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,5 +1,6 @@
 require 'station'
 describe Station do
+
   subject(:station){described_class.new(name: "Old Street", zone: 1)}
   it 'knows its name' do
     expect(station.name).to eq("Old Street")
@@ -10,4 +11,3 @@ describe Station do
   end
 
 end
-
